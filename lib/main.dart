979089dd910949fc0e280c67c9bee584451e7703
Humza Khawar <humzakhawar@gmail.com>
2022-02-11 16:57:06 +0500

    Added Stateful widget and added changing image function on dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -13,19 +14,46 @@ void main() {
   ));
 }
 
-class DiceApp extends StatelessWidget {
-  // This widget is the root of your application.
+class DiceApp extends StatefulWidget {
+  const DiceApp({Key? key}) : super(key: key);
+
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftDiceNumber = 5, rightDiceNumber = 6;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-              child: TextButton(
-                  onPressed: () {}, child: Image.asset('images/dice1.png'))),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+
+                  leftDiceNumber = Random().nextInt(6)+1;
+                  rightDiceNumber = Random().nextInt(6)+1;
+                },);
+
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
           Expanded(
-              child: TextButton(
-                  onPressed: () {}, child: Image.asset('images/dice1.png'))),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+
+                  leftDiceNumber = Random().nextInt(6)+1;
+                  rightDiceNumber = Random().nextInt(6)+1;
+                },);
+
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
         ],
       ),
     );
